bab2e294f769c93759ea4d0c23505c7c61d079a8
jackson <jackson@jacksons-MacBook-Pro.local>
2018-08-24 23:03:35 +0800

    Jackson/Zhipeng Game Dog IOS

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,17 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var countUILabel: UILabel!
+    var count = 0
+    
+    @IBAction func increaseButtonClick(_ sender: Any) {
+         count += 1
+        countUILabel.text = String(count)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
